f9690dcf8913f8b1cbf63f5a592dc45f3a49fffb
a-marzouk01 <a.marzouk01@outlook.com>
2026-05-06 13:56:30 -0400

    better RNG

--- templates/misc/random.cpp
+++ templates/misc/random.cpp
@@ -3,15 +3,8 @@
 typedef long long ll; 
 using namespace std;
 
-ll rng() {
-    static mt19937 gen(
-            chrono::steady_clock::now().time_since_epoch().count());
-    return uniform_int_distribution<long long>(0, INT64_MAX)(gen);
-}
-
-ll rng(ll mn, ll mx) {
-    static mt19937 gen(
-            chrono::steady_clock::now().time_since_epoch().count());
-    return uniform_int_distribution<long long>(mn, mx)(gen);
+mt19937 mt(time(NULL));
+int rng(int mn, int mx) {
+    return mn + (mt() % (mx - mn + 1));
 }
 
